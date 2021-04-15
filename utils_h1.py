413b91b0ba6e33a2967c8e22374e2e95380c1503
Pranav Deo <pranavdeo99@gmail.com>
2021-04-15 20:10:17 +0530

    Adding comments

--- utils_h1.py
+++ utils_h1.py
@@ -1,8 +1,8 @@
 
 def min_latency(Vs, v0, num_uav, Gm, Gc):
     """
-    Input:  Vs -> list of SL node nmbers
-            v0 -> Initial node number
+    Input:  Vs -> list of SL node numbers
+            v0 -> Base node number
             num_uav -> Number of uav available
             Gm -> Movement Graph
             Gc -> Communication Graph
@@ -17,20 +17,56 @@ def min_latency(Vs, v0, num_uav, Gm, Gc):
 
 
 def solve_tsp(Vs, Gm):
+    """
+    Input:  Vs -> list of SL node nmbers
+            Gm -> Movement Graph
+
+    Output: T -> Tour path (List of nodes in the path)
+    """
     pass
 
 
 def split_tour(T, k):
+    """
+    Input:  T -> Tour path (List of nodes in the path)
+            k -> number of uav in the path
+
+    Output: tours_array -> List of lists of tours (i.e. list of nodes in the path)
+    """
     pass
 
 
 def distGM(ev, sv):
+    """
+    Input:  ev -> end node
+            sv -> start node
+            Gm -> Movement Graph
+
+    Output: len_path -> length of the shortest path between ev and sv in Gm 
+    """
     pass
 
 
 def minmax_matching(A):
+    """
+    Input:  A -> 2D matrix of PnC of all start and end nodes
+
+    Output: M -> A list of UAVs and their start vertices
+    """
     pass
 
 
 def mlp(v, v0, gamma_v, Gm, Gc):
+    """
+    Input:  v -> node number
+            v0 -> Base node number
+            gamma_v -> Number of uav to use
+            Gm -> Movement Graph
+            Gc -> Communication Graph
+
+    Output: sv -> list of start vertex for each UAV
+            ev -> list of end vertex for each UAV
+            st -> list of start time for each UAV
+            et -> list of end time for each UAV
+    """
     pass
