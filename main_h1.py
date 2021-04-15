8fece741ef9506ad1beefb395b8c03d2c2ed68e0
nilbarde <nihalbarde@gmail.com>
2021-04-16 04:15:00 +0530

    small changes

--- main_h1.py
+++ main_h1.py
@@ -55,8 +55,10 @@ def give_m1(Gm, Gc, Vs, r, Lc, V0):
     k = r // gamma
 
     optimal_path_in_points_idxs, optimal_path_in_sets_idxs, optimal_cost, distance_matrix = solve_tsp(Vs, Gm)
+    print("----")
+    print(optimal_path_in_points_idxs)
+    exit()
     tour_array = split_tour(optimal_path_in_points_idxs, k, Lc, distance_matrix)
-
     for ii in range(1, k + 1):
         Ri = list(range((ii - 1) * gamma + 1, ii * gamma + 1))
         v_ = V0
@@ -82,3 +84,6 @@ def give_m1(Gm, Gc, Vs, r, Lc, V0):
             st_array[np.argmin(st_array)] = min(old_st_array) + min([old_et_array[mm] + distGM(Gm, old_ev_array[mm], sv_array[mm]) for mm in Ri])
             v_ = v
             old_ev_array, old_st_array, old_et_array = ev_array, st_array, et_array
+
+
+give_m1(Gm, Gc, Vs, r, Lc, V0)
