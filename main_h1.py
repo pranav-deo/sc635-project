3825bcbb13bb2e404e0e16cdb03ab51d904e0bf6
Pranav Deo <pranavdeo99@gmail.com>
2021-04-19 19:19:00 +0530

    trash

--- main_h1.py
+++ main_h1.py
@@ -39,6 +39,7 @@ def give_m1(Gm, Gc, Vs, r, Lc, V0):
     for num_uav in range(r, 0, -1):
         # All MLPs from all v ∈ Vs to the BS with i UAVs:
         dist_array = min_latency(Vs, V0, num_uav, Gm, Gc)
+
         for ii in range(len(Vs)):
             if dist_array[ii] <= Lc:
                 gammas[ii] = num_uav
@@ -60,7 +61,8 @@ def give_m1(Gm, Gc, Vs, r, Lc, V0):
     for ii in range(1, k + 1):
         Ri = list(range((ii - 1) * gamma + 1, ii * gamma + 1))
         v_ = V0
-        _, old_ev_array, old_st_array, old_et_array = mlp(v_, V0, gammas[v_], Gm, Gc)
+        old_sv_array, old_ev_array, old_st_array, old_et_array = mlp(v_, V0, gammas[v_], Gm, Gc)
+        stv_ = 0
         for v in tour_array[num_uav]:
             (sv_array, ev_array, st_array, et_array) = mlp(v, V0, gammas[v], Gm, Gc)
 
@@ -70,7 +72,7 @@ def give_m1(Gm, Gc, Vs, r, Lc, V0):
 
             for ll in range(1, gammas[v]):
                 for mm in Ri:
-                    A[ll][mm] = min(old_st_array) + old_et_array[mm] + distGM(Gm, old_ev_array[mm] + sv_array[ll])
+                    A[ll][mm] = stv_ + old_et_array[mm] + distGM(Gm, old_ev_array[mm] + sv_array[ll])
 
             M = minmax_matching(A)
 
@@ -79,9 +81,9 @@ def give_m1(Gm, Gc, Vs, r, Lc, V0):
                 ev_array[mm] = ev_array[M[mm]]
                 st_array[mm] = st_array[M[mm]]
                 et_array[mm] = et_array[M[mm]]
-            st_array[np.argmin(st_array)] = min(old_st_array) + min([old_et_array[mm] + distGM(Gm, old_ev_array[mm], sv_array[mm]) for mm in Ri])
+            stv_ = stv_ + min([old_et_array[mm] + distGM(Gm, old_ev_array[mm], sv_array[mm]) for mm in Ri])
             v_ = v
-            old_ev_array, old_st_array, old_et_array = ev_array, st_array, et_array
+            old_ev_array, old_et_array = ev_array, et_array
 
 
 give_m1(Gm, Gc, Vs, r, Lc, V0)
